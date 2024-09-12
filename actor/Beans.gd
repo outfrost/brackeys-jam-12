c4504c47e49d4a5a655a6603d80d07488617008e
outfrost <kotlet.bahn@gmail.com>
2024-09-12 05:22:59 +0200

    Beans rotates. somehow

--- actor/Beans.gd
+++ actor/Beans.gd
@@ -4,7 +4,12 @@ const SPEED: float = 1.0
 const ACCEL: float = 8.0
 const JUMP_VELOCITY: float = 3.0
 
+var enabled: = true
+
 func _physics_process(delta: float) -> void:
+	if !enabled:
+		return
+
 	if !is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -12,7 +17,28 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir: = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction: = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction: = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, 0.125 * TAU).normalized()
+
+	if direction.length_squared() > 0.05:
+		var angle: = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP)
+		var slerp_rate: float = clamp(8.0 * (0.25 + 0.5 * (cos(angle - rotation.y) + 1.0)) * delta, 0.0, 1.0)
+		rotation.y = lerp_angle(rotation.y, angle, slerp_rate)
+
+	#rotation
+	#var direction_local: = transform.basis * direction
+	#if direction_local.length_squared() > 0.05:
+		#var angle: = Vector3.FORWARD.signed_angle_to(direction_local, Vector3.UP)
+		#DebugOverlay.display(str(angle))
+#
+		#var slerp_rate: float = clamp(8.0 * (0.25 + 0.5 * (cos(angle) + 1.0)) * delta, 0.0, 1.0)
+		#var new_rotation_y: = lerp_angle(rotation.y, rotation.y + angle, slerp_rate)
+#
+		## prevent jitter/wiggle at low physics framerates
+		#if abs(angle_difference(rotation.y, new_rotation_y)) > abs(angle):
+			#new_rotation_y = rotation.y + angle
+#
+		#rotation.y = new_rotation_y
+
 	velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCEL * delta)
 	velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCEL * delta)
 
